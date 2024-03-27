b61d0ceb284ba573efb83ac4dc459a5e3f06e060
Steven Nguyen <stnguyen90@users.noreply.github.com>
2024-03-27 11:54:49 -0700

    chore(app): install tanstack router for routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), TanStackRouterVite()],
 });
